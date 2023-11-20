1b6b84513f2b522d8c5e29dfeae2411904e25134
BrunoZambuze <zambuzesilva@hotmail.com>
2023-11-19 21:55:40 -0300

    Criacao das entidades e string de conexao

---
+++ APICatalogo/Context/ApiCatalogoContext.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class ApiCatalogoContext : DbContext
+    {
+        public ApiCatalogoContext(DbContextOptions<ApiCatalogoContext> options) : base(options)
+        {
+        }
+
+        //Definir o mapeamento das entidades  nas tabelas
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
---
+++ APICatalogo/Program.cs
@@ -0,0 +1,30 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Inserir a string que conexão
+string SqlConnection = builder.Configuration.GetConnectionString("ConexaoPadrao");
+
+builder.Services.AddDbContext<ApiCatalogoContext>(options => options.UseMySql(SqlConnection, 
+    ServerVersion.AutoDetect(SqlConnection)));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
