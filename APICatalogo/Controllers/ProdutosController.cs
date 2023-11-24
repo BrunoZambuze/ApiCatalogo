873dd59980a6792dcac404143db4747d89981493
BrunoZambuze <zambuzesilva@hotmail.com>
2023-11-23 22:57:42 -0300

    Adicionado Controller e leitura de todos os produtos cadastrados

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using APICatalogo.Context;
+using APICatalogo.Domain;
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ApiCatalogoContext _context;
+        public ProdutosController(ApiCatalogoContext context)
+        {
+            _context = context;
+        }
+
+        //Action que retorna todos os produtos
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados...");
+            }
+            return produtos;
+        }
+    }
+}
