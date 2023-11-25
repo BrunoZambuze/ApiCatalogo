a55faba01595b7845eb0d8704b4db21e144c17c7
BrunoZambuze <zambuzesilva@hotmail.com>
2023-11-25 16:21:52 -0300

    Adicionado controller e actions das categorias

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,89 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ApiCatalogoContext _context;
+        public CategoriasController(ApiCatalogoContext context)
+        {
+            _context = context;
+        }
+
+        //Action que retorna todas as categorias
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categoria = _context.Categorias.ToList();
+            if (categoria is null)
+            {
+                return NotFound("Categoria não encontrada...");
+            }
+            return categoria;
+        }
+
+        //Action que retorna todos os produtos da categoria
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            return _context.Categorias.Include(p => p.Produtos).ToList();
+        }
+
+        //Action que retorna a categoria pelo id
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Categoria não encontrada...");
+            }
+            return Ok(categoria);
+        }
+
+        //Action para criar uma nova categoria
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null)
+            {
+                return BadRequest();
+            }
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId}, categoria);
+        }
+
+        //Action para atualizar uma categoria
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+
+        //Action para deletar uma categoria
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Categoria não encontrada...");
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return Ok(categoria);
+        }
+    }
+}
